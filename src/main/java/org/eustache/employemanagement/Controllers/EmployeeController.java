9959969c1b13f44eca61535753032fc0ac731cd4
eustachekamala <eustachejared25@gmail.com>
2025-09-15 23:06:35 +0300

    created the RESt controller of the Employee

---
+++ src/main/java/org/eustache/employemanagement/Controllers/EmployeeController.java
@@ -0,0 +1,42 @@
+package org.eustache.employemanagement.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.eustache.employemanagement.DTOs.Requests.EmployeeRequestDTO;
+import org.eustache.employemanagement.DTOs.Responses.EmployeeSummaryDTO;
+import org.eustache.employemanagement.Services.EmployeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<EmployeeSummaryDTO>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeSummaryDTO> getEmployeeById(@PathVariable Integer id) {
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<String> insertEmployee(@RequestBody EmployeeRequestDTO employee) {
+        return ResponseEntity.ok(  employeeService.createEmployee(employee));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateEmployee(@PathVariable Integer id, @RequestBody EmployeeRequestDTO employee) {
+        return ResponseEntity.ok(employeeService.updateEmployee(id, employee));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Integer id) {
+        return ResponseEntity.ok(employeeService.deleteEmployee(id));
+    }
+}
