78c992a48191bc26643e9edbaa68ab170a5211d4
eustachekamala <eustachejared25@gmail.com>
2025-10-07 11:49:08 +0300

    updated the accessors of the attendance entity

--- src/main/java/org/eustache/employemanagement/models/Attendance.java
+++ src/main/java/org/eustache/employemanagement/models/Attendance.java
@@ -2,13 +2,18 @@ package org.eustache.employemanagement.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Data
+@Getter
+@Setter
 @Entity
+@RequiredArgsConstructor
 public class Attendance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
