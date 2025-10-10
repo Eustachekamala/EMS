9d6f0b5f63069ec39937def86b5937d901541291
eustachekamala <eustachejared25@gmail.com>
2025-10-10 22:10:29 +0300

    updated the accessor of the address Entity

--- src/main/java/org/eustache/employemanagement/models/Address.java
+++ src/main/java/org/eustache/employemanagement/models/Address.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
 @Entity
 public class Address {
     @Id
