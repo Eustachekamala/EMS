d3cff81a04e2f511633e271bc7b6cf456cf8f571
eustachekamala <eustachejared25@gmail.com>
2025-10-21 16:12:19 +0300

    updated the yml file

--- src/main/java/org/eustache/employemanagement/Config/SecurityConfig.java
+++ src/main/java/org/eustache/employemanagement/Config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package org.eustache.employemanagement.Config;
 
-import java.security.Security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
